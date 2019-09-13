648d258678777208fbae37c94b99f64c41b8a283
Emmanuel <femmor2u@gmail.com>
2019-09-13 16:11:53 -0500

    broken the homepage into smaller components, finished styling the menu item components

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// Components
+import Directory from "../../components/directory/directory.component";
+
+// Styles
+import "./homepage.styles.scss";
+
+const Homepage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default Homepage;
