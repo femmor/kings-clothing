e2d5484a4dfd7bab6a4f9081de9bcb22b66f1dcc
Emmanuel <femmor2u@gmail.com>
2019-09-16 14:44:47 -0500

    updated the menu-item to navigate with withRouter Ract HOC function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
 
 // Components
 import Homepage from "./pages/homepage/homepage.component";
+import HatsPage from "./pages/hats/hats.component";
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => {
   return (
     <div
       style={{
         backgroundImage: `url(${imageUrl})`
       }}
       className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
     >
       <div
         className="background-image"
@@ -24,4 +26,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
